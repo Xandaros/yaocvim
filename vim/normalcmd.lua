7572fadd4ba51bd7bf6f42d2e7e55fa6708a816c
Martin Zeller <mz.bremerhaven@gmail.com>
2020-05-30 22:31:50 +0200

    Add b and e motions

--- vim/normalcmd.lua
+++ vim/normalcmd.lua
@@ -185,35 +185,76 @@ registerMotion({
     end
 })
 
+local function findNextWord(str, pos)
+    if pos == nil then return nil end
+    local word1 = "[%a%d_]"
+    local word2 = "[^%a%d_%s]"
+    local cur_char = str:sub(pos, pos)
+    local after_cursor = str:sub(pos, #str)
+    local patterns = {"[^%s]"}
+
+    if string.match(cur_char, word1) then
+        patterns = {word2, "%s" .. word1}
+    elseif string.match(cur_char, word2) then
+        patterns = {word1, "%s" .. word2}
+    end
+    local idx = nil
+    for _, pattern in ipairs(patterns) do
+        local i, _ = string.find(after_cursor, pattern)
+        if i ~= nil then
+            if idx == nil or i < idx then
+                idx = i
+            end
+        end
+    end
+    if idx == nil then
+        return nil
+    end
+    local new_x = pos + idx - 1
+    if string.match(str:sub(new_x, new_x), "%s") then
+        new_x = new_x + 1
+    end
+    return new_x
+end
+
+local function findEndOfWord(str, pos)
+    if pos == nil then return nil end
+    local word1 = "[%a%d_]"
+    local word2 = "[^%a%d_%s]"
+    local cur_char = str:sub(pos, pos)
+    local after_cursor = str:sub(pos + 1, #str) .. " "
+    local patterns = {
+        "[%a%d_][^%a%d_]",
+        "[^%a%d_%s][%a%d_%s]"
+    }
+
+    local idx = nil
+    for _, pattern in ipairs(patterns) do
+        local i, _ = string.find(after_cursor, pattern)
+        if i ~= nil then
+            if idx == nil or i < idx then
+                idx = i
+            end
+        end
+    end
+    if idx == nil then
+        return nil
+    end
+
+    return pos + idx
+end
+
 registerMotion({
     key = "w",
     linewise = false,
     exclusive = true,
     jump = false,
     execute = function(window)
-        local word1 = "[%a%d_]"
-        local word2 = "[^%a%d_%s]"
         local cursor = window.cursor
         local cur_line = window.buffer.content[cursor[2]]
-        local cur_char = cur_line:sub(cursor[1], cursor[1])
-        local after_cursor = cur_line:sub(cursor[1], #cur_line)
-        local patterns = {"[^%s]"}
+        local next_word = findNextWord(cur_line, cursor[1])
 
-        if string.match(cur_char, word1) then
-            patterns = {word2, "%s" .. word1}
-        elseif string.match(cur_char, word2) then
-            patterns = {word1, "%s" .. word2}
-        end
-        local idx = nil
-        for _, pattern in ipairs(patterns) do
-            local i, _ = string.find(after_cursor, pattern)
-            if i ~= nil then
-                if idx == nil or i < idx then
-                    idx = i
-                end
-            end
-        end
-        if idx == nil then
+        if next_word == nil then
             -- no words found on this line, jump to next line's first non-empty char
             local next_line = window.buffer.content[cursor[2] + 1]
             if next_line == nil then
@@ -221,11 +262,53 @@ registerMotion({
             end
             return {util.firstNonBlank(next_line), cursor[2] + 1}
         end
-        local new_x = cursor[1] + idx - 1
-        if string.match(cur_line:sub(new_x, new_x), "%s") then
-            new_x = new_x + 1
+        return {next_word, cursor[2]}
+    end
+})
+
+registerMotion({
+    key = "e",
+    linewise = false,
+    exclusive = false,
+    jump = false,
+    execute = function(window)
+        local cursor = window.cursor
+        local x = cursor[1]
+        for i=cursor[2], #window.buffer.content do
+            local cur_line = window.buffer.content[i]
+            local word_end = findEndOfWord(cur_line, x)
+            if word_end ~= nil then
+                return {word_end, i}
+            end
+            x = 1
         end
-        return {new_x, cursor[2]}
+        return {#window.buffer.content[#window.buffer.content], #window.buffer.content}
+    end
+})
+
+registerMotion({
+    key = "b",
+    linewise = false,
+    exclusive = true,
+    jump = false,
+    execute = function(window)
+        local cursor = window.cursor
+
+        local x = cursor[1]
+        for i=cursor[2], 1, -1 do
+            local cur_line = window.buffer.content[i]
+            if x == nil then
+                x = #cur_line
+            end
+            local reversed = cur_line:reverse()
+            local wordEnd_rev = findEndOfWord(reversed, #cur_line - x + 1)
+            if wordEnd_rev ~= nil then
+                local ret = #cur_line - wordEnd_rev + 1
+                return {ret, i}
+            end
+            x = nil
+        end
+        return cursor
     end
 })
 
