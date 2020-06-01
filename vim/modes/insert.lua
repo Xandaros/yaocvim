3c9a4868d1b9376bd2a4dbb4eb9d1a3a64e952b2
Martin Zeller <mz.bremerhaven@gmail.com>
2020-06-01 21:59:34 +0200

    Support newlines in insert mode

--- vim/modes/insert.lua
+++ vim/modes/insert.lua
@@ -19,11 +19,18 @@ function mod.keyPress(charcode, keycode)
     local cursor = window.cursor
     local buffer = window.buffer
     local char = string.char(charcode)
+    local line = buffer.content[cursor[2]]
     if charcode >= 32 and charcode <= 126 then
-        local line = buffer.content[cursor[2]]
         line = line:sub(1, cursor[1] - 1) .. char .. line:sub(cursor[1])
         buffer.content[cursor[2]] = line
         cursor[1] = cursor[1] + 1
+    elseif char == "\r" or char == "\n" then
+        local next_line = line:sub(cursor[1])
+        line = line:sub(1, cursor[1] - 1)
+        buffer.content[cursor[2]] = line
+        table.insert(buffer.content, cursor[2] + 1, next_line)
+        cursor[2] = cursor[2] + 1
+        cursor[1] = 1
     elseif charcode == 27 or charcode == 0 and keycode == keyboard.keys.f1 then
         shared.setMode(require("vim/modes/normal"))
         status.setStatus("")
